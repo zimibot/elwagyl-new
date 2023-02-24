cb8501e391d44d41d3b6c82d88d5bf9215550f48
Harris <zimibot@gmail.com>
2023-02-24 23:29:00 +0700

    -

--- public/electron.js
+++ public/electron.js
@@ -6,9 +6,11 @@ const Ping = require("ping")
 
 async function checkInternet(cb) {
 
-  let res = await Ping.promise.probe("10.22.22.6", {
+  let res = await Ping.promise.probe("google.com", {
     timeout: 10,
   });
+
+  console.log(res)
   cb(res.alive)
 
 }
@@ -16,28 +18,56 @@ async function checkInternet(cb) {
 
 // app.disableHardwareAcceleration()
 
-
-app.whenReady().then(() => checkInternet(function (isConnected) {
-  if (isConnected) {
-    try {
-      CreateWindow({ urlCurrent: false, prodUrl: "../index.html", maximize: true })
-    } catch (error) {
-      console.log(error)
-      app.quit()
+const Loading = () => {
+  let notif = CreateWindow({
+    urlCurrent: "http://localhost:8000/html/loading/index.html",
+    prodUrl: "../html/loading/index.html",
+    config: {
+      width: 250,
+      height: 250,
+      minWidth: 250,
+      minHeight: 250,
+      maxWidth: 250,
+      maxHeight: 250,
+      transparent: true,
+      backgroundColor: false,
+    },
+    webConfig: {
+      zoomFactor: 1.0
     }
-  } else {
-    notif({
-      title: "ERROR NETWORK",
-      content: "YOU ARE OFFLINE, PLEASE CHECK YOUR INTERNET NETWORK OR VPN NETWORK",
-      colorContent: "#ED6A5E",
-      favicon: `icon/alert.png`,
-      iconContent: `<svg width="40" height="40" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 12H10V7H12M12 16H10V14H12M0 19H22L11 0L0 19Z" fill="#ED6A5E"/>
-      </svg>
-      `
-    })
-  }
-}));
+  })
+
+  return notif
+}
+
+
+app.whenReady().then(() => {
+  let load = Loading()
+  checkInternet(function (isConnected) {
+    if (isConnected) {
+      try {
+        CreateWindow({ urlCurrent: false, prodUrl: "../index.html", maximize: true })
+        load.close()
+      } catch (error) {
+        app.quit()
+      }
+    } else {
+      notif({
+        title: "ERROR NETWORK",
+        content: "YOU ARE OFFLINE, PLEASE CHECK YOUR INTERNET NETWORK OR VPN NETWORK",
+        colorContent: "#ED6A5E",
+        favicon: `icon/alert.png`,
+        iconContent: `<svg width="40" height="40" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 12H10V7H12M12 16H10V14H12M0 19H22L11 0L0 19Z" fill="#ED6A5E"/>
+        </svg>
+        `
+      })
+      load.close()
+    }
+
+  })
+}
+);
 app.commandLine.appendSwitch('high-dpi-support', 1)
 app.commandLine.appendSwitch('force-device-scale-factor', 1)
 app.on('browser-window-focus', function () {
--- public/function/other.view.js
+++ public/function/other.view.js
@@ -2,36 +2,41 @@ const OtherView = require('./browser.oher.window')
 const { ipcMain } = require('electron')
 
 module.exports = function OtherViewBrowser(win) {
-    let mainBrowser = win
-    let viewBrowser = OtherView()
+    try {
+        let mainBrowser = win
+        let viewBrowser = OtherView()
 
 
-    ipcMain.handle('routes', async (event, arg) => {
-        return new Promise(function () {
-            // do stuff
-            if (true) {
-                if (arg.url) {
-                    try {
-                        mainBrowser.setBrowserView(viewBrowser)
-                        // mainBrowser.addBrowserView(viewBrowser)
-                        // console.log(arg)
-                        viewBrowser.setBounds({ x: 0, ...arg.size })
-                        viewBrowser.setAutoResize({
-                            width: true,
-                            height: true
-                        })
-                        viewBrowser.webContents.loadURL(`https://${arg.url}`)
-                    } catch (error) {
-                        console.log(error)
+        ipcMain.handle('routes', async (event, arg) => {
+            return new Promise(function () {
+                // do stuff
+                if (true) {
+                    if (arg.url) {
+                        try {
+                            mainBrowser.setBrowserView(viewBrowser)
+                            // mainBrowser.addBrowserView(viewBrowser)
+                            // console.log(arg)
+                            viewBrowser.setBounds({ x: 0, ...arg.size })
+                            viewBrowser.setAutoResize({
+                                width: true,
+                                height: true
+                            })
+                            viewBrowser.webContents.loadURL(`https://${arg.url}`)
+                        } catch (error) {
+                            console.log(error)
+                        }
+
+                    } else {
+                        mainBrowser.removeBrowserView(viewBrowser)
                     }
-                   
-                } else {
-                    mainBrowser.removeBrowserView(viewBrowser)
-                }
 
-            }
+                }
+            });
         });
-    });
+
+    } catch (error) {
+        console.log(error)
+    }
 
 
 }
