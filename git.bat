b7aebc0f66a855d4e8413d01fffed8cb677eb2b9
zimibot <zimibot@gmail.com>
2023-04-28 14:51:48 +0700

    '-'

--- git.bat
+++ git.bat
@@ -1,3 +1,3 @@
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -am '-'
-"C:\Program Files\Git\bin\git.exe" push
+"C:\Program Files\Git\bin\git.exe" push origin main
