bf252ba7312be77d83c1b6cf0809174e2cebf35b
zimibot <zimibot@gmail.com>
2023-05-04 23:44:57 +0700

    'funtsu_Thu_05_04'

--- git.bat
+++ git.bat
@@ -2,7 +2,7 @@
 FOR /F "tokens=1-3 delims=/ " %%A IN ('DATE /T') DO (
     SET DAY=%%A
     SET MONTH=%%B
-    SET YEAR=%%C-1
+    SET YEAR=%%C
 )
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -am 'funtsu_%DAY%_%MONTH%_%YEAR%'
