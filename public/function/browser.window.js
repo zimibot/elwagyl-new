8267dbfca9713e978c3bbfe282bd10ce023c3220
zimibot <zimibot@gmail.com>
2023-05-04 20:35:08 +0700

    '-'

--- public/function/browser.window.js
+++ public/function/browser.window.js
@@ -1,23 +1,44 @@
-
 const { initialize, enable } = require('@electron/remote/main');
 const { BrowserWindow, screen } = require('electron');
 const path = require('path');
 
+// Disable security warnings
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
-
-
+// Initialize remote module
 initialize();
 
 
-module.exports = function WindowConfig(mainConfig = {}, webPreferences = {}) {
+// Create window configuration function
+module.exports = function createWindowConfig(mainConfig = {}, webPreferences = {}) {
 
-  let innerScreen = screen.getPrimaryDisplay()
-  let size = innerScreen.size
+  // Define screen resolutions
+  const resolutions = {
+    HD: { width: 1280, height: 720, zoomFactor: 0.8 },
+    UHD: { width: 1920, height: 1080, zoomFactor: 1 },
+    "2k": { width: 2560, height: 1440, zoomFactor: 1.3 },
+    "4k": { width: 3840, height: 2160, zoomFactor: 1.7 }
+  };
 
-  const win = new BrowserWindow({
-    minWidth: 1500,
-    minHeight: 600,
+  // Get primary display size
+  const { width, height } = screen.getPrimaryDisplay().size;
+
+  // Set screen resolution based on display width
+  let screenResolution = "HD";
+  if (width >= resolutions["4k"].width) {
+    screenResolution = "4k";
+  } else if (width >= resolutions["2k"].width) {
+    screenResolution = "2k";
+  } else if (width >= resolutions.UHD.width) {
+    screenResolution = "UHD";
+  }
+
+  // Set zoom factor based on screen resolution
+  const zoomFactor = resolutions[screenResolution].zoomFactor;
+
+  const windowConfig = {
+    minWidth: resolutions[screenResolution].width,
+    minHeight: resolutions[screenResolution].height,
     show: false,
     autoHideMenuBar: true,
     frame: false,
@@ -28,14 +49,15 @@ module.exports = function WindowConfig(mainConfig = {}, webPreferences = {}) {
       nodeIntegration: false, // is default value after Electron v5
       contextIsolation: true, // protect against prototype pollution
       enableRemoteModule: true,
-      zoomFactor: size.width < 1500 ? 0.64 : size.width < 2000 ? 0.89 : size.width < 3000 ? 1.1 : 1.4,
+      zoomFactor,
       preload: path.join(__dirname, "../preload.js"), // path to your preload.js file
       ...webPreferences,
-
     },
-  });
+  };
 
-  enable(win)
+  // Create and enable remote module for window
+  const window = new BrowserWindow(windowConfig);
+  enable(window);
 
-  return win
-}
+  return window;
+};
