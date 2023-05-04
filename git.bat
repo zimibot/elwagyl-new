739ce0c273a43740702defb7b4d6adaf2b624a45
zimibot <zimibot@gmail.com>
2023-05-04 23:41:25 +0700

    'funtsu-04'

--- git.bat
+++ git.bat
@@ -1,7 +1,6 @@
-echo "start"
+@echo off
+FOR /F "tokens=1-3 delims=/ " %%A IN ('DATE /T') DO SET YEAR=%%C
 
 "C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -am '-'
+"C:\Program Files\Git\bin\git.exe" commit -am 'funtsu-%YEAR%'
 "C:\Program Files\Git\bin\git.exe" push origin main
-
-echo "success"
--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,8 @@ import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-
         <App />
-
         <Toaster></Toaster>
-
         </>
 
 )
