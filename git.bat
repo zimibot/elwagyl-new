174b1a45d46e5183bc43e4033f31f45685f86e1c
zimibot <zimibot@gmail.com>
2023-05-04 23:44:29 +0700

    'funtsu_Thu_05_04'

--- git.bat
+++ git.bat
@@ -1,10 +1,9 @@
 @echo off
-FOR /F "tokens=1-3 delims=:." %%A IN ("%TIME%") DO (
-    SET HOUR=%%A
-    SET MINUTE=%%B
-    SET SECOND=%%C
+FOR /F "tokens=1-3 delims=/ " %%A IN ('DATE /T') DO (
+    SET DAY=%%A
+    SET MONTH=%%B
+    SET YEAR=%%C
 )
-
 "C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -am 'funtsu_%HOUR%%MINUTE%%SECOND%'
+"C:\Program Files\Git\bin\git.exe" commit -am 'funtsu_%DAY%_%MONTH%_%YEAR%'
 "C:\Program Files\Git\bin\git.exe" push origin main
