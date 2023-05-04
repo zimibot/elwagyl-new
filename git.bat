cd3a045c07e5dc8fce75bc2f012c99113395a8f9
zimibot <zimibot@gmail.com>
2023-05-04 23:46:07 +0700

    'funtsu_Thu_05_2004'

--- git.bat
+++ git.bat
@@ -4,6 +4,10 @@ FOR /F "tokens=1-3 delims=/ " %%A IN ('DATE /T') DO (
     SET MONTH=%%B
     SET YEAR=%%C
 )
+
+REM Add 2000 to the year if it's less than 100
+IF %YEAR% LSS 100 SET YEAR=20%YEAR%
+
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -am 'funtsu_%DAY%_%MONTH%_%YEAR%'
 "C:\Program Files\Git\bin\git.exe" push origin main
